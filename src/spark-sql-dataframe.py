41166c35267e0c3f765efaa7b1f5fe5cee1361d5
Kohei <ohayousagi.ac.kook0727@gmail.com>
2020-09-12 22:17:04 +0900

    master

--- src/spark-sql-dataframe.py
+++ src/spark-sql-dataframe.py
@@ -3,21 +3,26 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("SparkSQL").getOrCreate()
 
 people = spark.read.option("header", "true").option("inferSchema", "true").csv("./dataset/fakefriends-header.csv")
-    
-print("Here is our inferred schema:")
-people.printSchema()
+people.createOrReplaceTempView('people')
+# schemaPeople = spark.createDataFrame(people).cache()
+# schemaPeople.createOrReplaceTempView("people")
 
-print("Let's display the name column:")
-people.select("name").show()
+# print("Here is our inferred schema:")
+# people.printSchema()
 
-print("Filter out anyone over 21:")
-people.filter(people.age < 21).show()
+spark.sql("SELECT * FROM people").show()
 
-print("Group by age")
-people.groupBy("age").count().show()
+# print("Let's display the name column:")
+# people.select("name").show()
 
-print("Make everyone 10 years older:")
-people.select(people.name, people.age + 10).show()
+# print("Filter out anyone over 21:")
+# people.filter(people.age < 21).show()
 
-spark.stop()
+# print("Group by age")
+# people.groupBy("age").count().show()
+
+# print("Make everyone 10 years older:")
+# people.select(people.name, people.age + 10).show()
+
+# spark.stop()
 
